aa17157ad16f7df0a07c576da55155a92f2adf55
Ostapenko Bogdan <bogdan.ostapenko.97@gmail.com>
2026-03-15 14:07:04 +0200

    add

--- src/server.js
+++ src/server.js
@@ -34,13 +34,12 @@ app.get('/notes/:noteId', (req, res) => {
   const id_param = req.params.noteId;
   res.status(200).json({ message: `Retrieved note with ID: ${id_param}` });
 });
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
-});
-
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
 });
 
 app.use((err, req, res, next) => {
